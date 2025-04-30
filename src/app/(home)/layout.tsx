b68133e02ece6e03f167cdb859eefef6d3def112
safi-okz <safi90655@gmail.com>
2025-04-30 18:15:11 +0500

    layout setting

---
+++ src/app/(home)/layout.tsx
@@ -0,0 +1,25 @@
+import Header from "@/components/shared/header";
+import Footer from "@/components/shared/footer";
+
+import { APP_DESCRIPTION, APP_NAME, APP_SLOGAN } from "@/lib/constants";
+export const metadata: Metadata = {
+  title: {
+    template: `%s | ${APP_NAME}`,
+    default: `${APP_NAME}. ${APP_SLOGAN}`,
+  },
+  description: APP_DESCRIPTION,
+};
+
+export default async function HomeLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-1 flex flex-col">{children}</main>
+      <Footer />
+    </div>
+  );
+}
