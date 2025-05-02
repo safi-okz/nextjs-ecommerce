1ff6065ef18ce6a8e5bc2f65f008f142b670e797
safi-okz <safi90655@gmail.com>
2025-05-02 16:35:00 +0500

    product data

--- types/index.ts
+++ types/index.ts
@@ -1,4 +1,18 @@
-import { z } from "zod";
-import { ProductInputSchema } from "@/lib/validator";
+import { ProductInputSchema } from '@/lib/validator'
+import { z } from 'zod'
 
 export type IProductInput = z.infer<typeof ProductInputSchema>
+export type Data = {
+  products: IProductInput[]
+  headerMenus: {
+    name: string
+    href: string
+  }[]
+  carousels: {
+    image: string
+    url: string
+    title: string
+    buttonCaption: string
+    isPublished: boolean
+  }[]
+}
